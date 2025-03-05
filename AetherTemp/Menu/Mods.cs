1ed011d8c48915ce443eef667fb80a0b184fea05
zyro <126006401+zyroyz@users.noreply.github.com>
2025-03-05 21:35:35 +0000

    Add files via upload

---
+++ AetherTemp/Menu/Mods.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BepInEx;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace StupidTemplate.Menu
+{
+    class mods
+    {
+        public static void Disconnect()
+        {
+            PhotonNetwork.Disconnect();
+        }
+
+        public static void orbitfast()
+        {
+            Main.orbitSpeed = 2f;
+        }
+
+        public static void orbitnormalspeed()
+        {
+            Main.orbitSpeed = 1f;
+        }
+
+        public static void orbitslow()
+        {
+            Main.orbitSpeed = 0.5f;
+        }
+
+        public static void orbitfar()
+        {
+            Main.orbitRadius = 1.5f;
+        }
+
+        public static void orbitnormal()
+        {
+            Main.orbitRadius = 1f;
+        }
+
+        public static void orbitclose()
+        {
+            Main.orbitRadius = 0.5f;
+        }
+        public static void placeholder()
+        {
+
+        }
+
+
+    }
+}
